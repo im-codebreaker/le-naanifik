30892683749bad7b900ea45d626e12704e8f00f5
MehdiHan <meh.bouchard@outlook.fr>
2022-04-20 08:22:15 +0200

    chore: add Dday active class base on datetime

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import './style.scss'
 
+const date: Date = new Date();
+const planningItems: NodeListOf<HTMLElement> | null = document.querySelectorAll('.planning_grid-item');
 const buttonBurger: HTMLButtonElement | null = document.querySelector('.navbar-burger');
 
 buttonBurger?.addEventListener('click', () => {
   buttonBurger.classList.toggle('open');
 })
+
+Array(planningItems)[0].forEach((item) => {
+  if (date.getDay().toString() === item.dataset?.date) {
+    item.classList.add('active')
+  } else {
+    item.classList.remove('active')
+  }
+}
+);
