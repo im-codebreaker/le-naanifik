ca497c220aa9248d860aa891577f24c295d112ba
MehdiHan <meh.bouchard@outlook.fr>
2022-04-22 12:14:30 +0200

    chore: add IntersectionObserver and backtop button

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,36 @@
 import './style.scss'
 
+interface ObserverOptions {
+  root: null,
+  rootMargin: string,
+  threshold: number
+}
+
 const date: Date = new Date();
 const planningItems: NodeListOf<HTMLElement> | null = document.querySelectorAll('.planning_grid-item');
 const buttonBurger: HTMLButtonElement | null = document.querySelector('.navbar-burger');
 
-buttonBurger?.addEventListener('click', () => {
-  buttonBurger.classList.toggle('open');
-})
+const options: ObserverOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0
+}
+
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry: any) => {
+    if (entry.isIntersecting) {
+      if (!entry.target.classList.contains('active')) {
+        entry.target.classList.add('active');
+        observer.unobserve(entry.target);
+      }
+    }
+  })
+}, options);
+
+let targets = document.querySelectorAll('.target');
+
+targets.forEach((target) => { observer.observe(target) })
+
 
 Array(planningItems)[0].forEach((item) => {
   if (date.getDay().toString() === item.dataset?.date) {
@@ -16,3 +40,8 @@ Array(planningItems)[0].forEach((item) => {
   }
 }
 );
+
+buttonBurger?.addEventListener('click', () => {
+  buttonBurger.classList.toggle('open');
+})
+
